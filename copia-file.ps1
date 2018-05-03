71c4704c86ad37a80cdb6088e28e9be48216627d
lucacaaaarotenuto <33749903+lucacaaaarotenuto@users.noreply.github.com>
2018-05-03 21:24:10 +0200

    Update copia-file.ps1

--- copia-file.ps1
+++ copia-file.ps1
@@ -1,31 +1,31 @@
-# new-item src -itemtype directory
+# new-item src -itemtype directory        
 
-$DS=$args[0]
+$DS=$args[0]    // directory sorgente
 
-$DD=$args[1]
-
-
-
-new-item $DD -itemtype directory
+$DD=$args[1]    // directory destinatario
+                // lettura degli argomenti passati e finiscono nelle variabili
 
 
-
-$cont=0;
-
-foreach ($file in Get-ChildItem $DS) {
-
-    $cont++;
-
-    # write-host $cont;
-
-    write-host "ora copio" $file $cont"_"$file;
-
-    $srcpath="$DS\$file";
-
-    $dstpath="$DD\$cont`_$file";
+new-item $DD -itemtype directory // DD viene definita come una directory
 
 
 
-    copy-item -path $srcpath -Destination $dstpath;
+$cont=0; // variabile contatore
+
+foreach ($file in Get-ChildItem $DS) { // per visualizzare tutti i file nella directory sorgente
+
+    $cont++; // incremento del contatore
+
+    # write-host $cont; // stampa del contatore
+
+    write-host "ora copio" $file $cont"_"$file; // stampa "ora copio nome file e files in nomefilenuovo"
+
+    $srcpath="$DS\$file";  
+
+    $dstpath="$DD\$cont"_"$file";  // il file sorgente cambierà nome quando verrà copiato nel file destinazione
+
+
+
+    copy-item -path $srcpath -Destination $dstpath; // copia dei file che si trovano nell'elemento di origine nella destinazione
 
 }
