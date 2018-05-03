1df4f915294dfda90080b74fa3d08478917462e8
lucacaaaarotenuto <33749903+lucacaaaarotenuto@users.noreply.github.com>
2018-05-03 22:31:55 +0200

    Add files via upload

---
+++ file_estensione.ps1
@@ -0,0 +1,33 @@
+# new-item src -itemtype directory
+
+$DS=$args[0]
+
+$DD=$args[1]
+
+$tipo="*.docx"
+
+
+
+new-item $DD -itemtype directory
+
+
+
+$cont=0;
+
+foreach ($file in Get-ChildItem $DS) {
+
+    $cont++;
+
+    # write-host $cont;
+
+    write-host "ora copio" $file $tipo $cont"_"$file;
+
+    $srcpath="$DS\$file $tipo";
+
+    $dstpath="$DD\$cont`_$file $tipo";
+
+
+
+    copy-item -path $srcpath -Destination $dstpath; 
+
+}
